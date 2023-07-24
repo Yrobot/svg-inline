17bc0fb0c7e49de1ae900e5e71210d75c7f488a5
Yrobot <Yrobot@qq.com>
2023-07-24 14:04:51 +0800

    add change color and opacity logic in demo

--- website/index.tsx
+++ website/index.tsx
@@ -2,27 +2,42 @@ import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
+const icons = ["svg-inline", "yrobot"];
+const fills = ["#C59FC9", "red", "green", "#C1E0F7", "black"];
+const opacities = [1, 0.4];
+
 function App() {
-  const [check, setCheck] = useState(true);
+  const [iconIndex, setIconIndex] = useState(0);
+  const [fillIndex, setFillIndex] = useState(0);
+  const [opacityIndex, setOpacityIndex] = useState(0);
   return (
     <div className="relative w-full h-[50vh] bg-slate-200 flex items-center justify-center flex-col space-y-8">
       <svg-inline
-        src={`/${check ? "svg-inline" : "yrobot"}.svg`}
+        src={`/${icons[iconIndex % icons.length]}.svg`}
         class="inline-block w-16 h-16 icon"
         style={{
-          "--svg-fill": "white",
-          "--svg-fill-opacity": 0.3,
+          "--svg-fill": fills[fillIndex % fills.length],
+          "--svg-fill-opacity": opacities[opacityIndex % opacities.length],
         }}
       ></svg-inline>
-      <button
-        className="px-3 py-2 rounded-lg bg-black text-white"
-        type="button"
-        onClick={() => {
-          setCheck(!check);
-        }}
-      >
-        Switch
-      </button>
+      <div className="space-x-4">
+        {[
+          { title: "Change Icon", callback: setIconIndex },
+          { title: "Change Color", callback: setFillIndex },
+          { title: "Change Opacity", callback: setOpacityIndex },
+        ].map(({ title, callback }) => (
+          <button
+            className="px-3 py-2 rounded-lg bg-black text-white"
+            type="button"
+            onClick={() => {
+              callback((old) => old + 1);
+            }}
+            key={title}
+          >
+            {title}
+          </button>
+        ))}
+      </div>
     </div>
   );
 }
